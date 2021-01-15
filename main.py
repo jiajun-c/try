d1d18bc8be81dfdb07fe45776c7467c201cd3788
Rambler <74438053+jiajun-c@users.noreply.github.com>
2021-01-15 16:27:41 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+for value1 in range(1,10):
+    small=[]
+    number=[]
+    number.append(value1)
+    if value1<=5:
+        small.append(value1)
+    print(small),
+
+
+
+
+
+
+
+
